8e27c9755d8372f34c20fab292d95be429455a34
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-11-26 08:56:59 +0400

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { routers } from './routers'
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={routers}/>
+)
